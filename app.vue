aa0c74ce18f0d4a23f00304a255bffd02fa19da9
MelnCat <melncatuwu@gmail.com>
2023-06-24 12:46:32 -0700

    do stuff

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+	</div>
+</template>
+
+<style lang="scss">
+@use "scss-reset/reset";
+</style>
---
+++ components/sidebar/Sidebar.vue
@@ -0,0 +1,10 @@
+<template>
+	<va-sidebar minimized minimized-width="2rem">
+		<va-sidebar-item active>
+			<va-sidebar-item-content>
+				<va-icon name="acunit" />
+				<va-sidebar-item-title> Title </va-sidebar-item-title>
+			</va-sidebar-item-content>
+		</va-sidebar-item>
+	</va-sidebar>
+</template>
